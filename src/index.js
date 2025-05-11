0ceb64ab01739269047de6354ab1b1a7f1fb93ca
Dragos Cirjan <dragos.cirjan@gmail.com>
2025-05-11 20:56:52 +0300

    chore: debug 1746986212

--- src/index.js
+++ src/index.js
@@ -85,7 +85,7 @@ const run = async () => {
       if (updatedWorkspacesTrees.length === 0) {
         logging.error('No workspaces found');
       }
-      logging.info(`Updated workspaces trees: ${JSON.stringify(updatedWorkspacesTrees)}`);
+      logging.info(`Updated workspaces trees -> Found ${JSON.stringify(updatedWorkspacesTrees)} main nodes`);
       logging.endGroup();
 
       //======================================================================
