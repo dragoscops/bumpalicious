715db50818c964a5793b357160b489f8297a985f
Dragos Cirjan <dragos.cirjan@gmail.com>
2025-05-17 01:39:39 +0300

    chore: debug 1747435179

--- src/workspace/python.js
+++ src/workspace/python.js
@@ -8,6 +8,7 @@ import {execa} from 'execa';
 import fs from 'fs-extra';
 import path from 'path';
 import * as logging from '../utils/logging.js';
+import {DEFAULT_VERSION} from './constants.js';
 
 /**
  * @typedef {Object} PythonConfig
@@ -23,15 +24,35 @@ import * as logging from '../utils/logging.js';
  * @returns {Promise<PythonConfig>} - Detected version
  */
 export const detect = async (projectPath) => {
-  const defaultName = path.basename(path.normalize(projectPath));
+  let name = path.basename(path.normalize(projectPath));
+  let version = DEFAULT_VERSION;
 
-  const pyprojectPath = path.join(projectPath, 'pyproject.toml');
-  if (await fs.pathExists(pyprojectPath)) {
-    try {
-      return detectJsonc(pyprojectPath, defaultName);
-    } catch (error) {
-      logging.error(`Error parsing ${file} file:`, error);
-    }
+  const pyprojectTomlPath = path.join(projectPath, 'pyproject.toml');
+  try {
+    await fs.access(pyprojectTomlPath);
+    const pyprojectData = await fs.readFile(pyprojectTomlPath, 'utf8');
+    const pyproject = toml.parse(pyprojectData);
+    return {
+      name,
+      version,
+      ...(pyproject?.project ?? {}),
+    };
+  } catch (error) {
+    logging.warning(`pyproject.toml not found or invalid in ${projectPath}; moving on...`, error);
+  }
+
+  const poetryTomlPath = path.join(projectPath, 'poetry.toml');
+  try {
+    await fs.access(poetryTomlPath);
+    const poetryData = await fs.readFile(poetryTomlPath, 'utf8');
+    const pyproject = toml.parse(poetryData);
+    return {
+      name,
+      version,
+      ...(pyproject?.tool?.poetry ?? {}),
+    };
+  } catch (error) {
+    logging.warning(`poetry.toml not found or invalid in ${projectPath}; moving on...`, error);
   }
 
   const setupMatches = {
@@ -43,17 +64,16 @@ export const detect = async (projectPath) => {
   // Check for setup.py and setup.cfg using regex
   for (const file of Object.keys(setupMatches)) {
     const filePath = path.join(projectPath, file);
-    if (await fs.pathExists(filePath)) {
-      try {
-        const content = await fs.readFile(filePath, 'utf8');
-        const versionMatch = content.match(setupMatches[file]);
-        return {
-          name: defaultName,
-          version: versionMatch ? versionMatch[1] : undefined,
-        };
-      } catch (error) {
-        logging.error(`Error parsing ${file} file:`, error);
-      }
+    try {
+      await fs.access(filePath);
+      const content = await fs.readFile(filePath, 'utf8');
+      const versionMatch = content.match(setupMatches[file]);
+      return {
+        name,
+        version: versionMatch ? versionMatch[1] : version,
+      };
+    } catch (error) {
+      logging.warning(`${file} not found or invalid in ${projectPath}; moving on...`, error);
     }
   }
 
@@ -65,38 +85,16 @@ export const detect = async (projectPath) => {
       {cwd: projectPath},
     );
     if (stdout.trim()) {
-      return stdout.trim();
+      return {
+        name,
+        version: stdout.trim(),
+      };
     }
   } catch {
-    // Ignore errors
+    logging.warning(`Unable to parse version, using python, in ${projectPath}; moving on...`, error);
   }
 
   logging.error(`No version file found in the NodeJS project at ${projectPath}`);
-};
-
-/**
- * Detect name and version from a Python toml project file
- *
- * @param {string} configPath
- * @param {string} defaultName
- * @returns {PythonConfig}
- */
-const detectJsonc = async (pyprojectPath, defaultName) => {
-  const content = await fs.readFile(pyprojectPath, 'utf8');
-  const pyprojectData = toml.parse(content);
-
-  if (pyprojectData?.tool?.poetry?.version) {
-    return {
-      name: pyprojectData.tool.poetry.name || defaultName,
-      version: pyprojectData.tool.poetry.version,
-    };
-  }
-  if (pyprojectData?.project?.version) {
-    return {
-      name: pyprojectData.project.name || defaultName,
-      version: pyprojectData.project.version,
-    };
-  }
 };
 
 /**
@@ -108,114 +106,72 @@ const detectJsonc = async (pyprojectPath, defaultName) => {
  * @param {string} options.newVersion - New version to set
  */
 export const updateVersion = async ({projectPath, newVersion}) => {
-  const pyprojectPath = path.join(projectPath, 'pyproject.toml');
-  if (await fs.pathExists(pyprojectPath)) {
-    try {
-      return updatePyProjectVersion(pyprojectPath, newVersion);
-    } catch (error) {
-      logging.error(`Failed to update Python project version: ${error.message}`);
-    }
+  const pyprojectTomlPath = path.join(projectPath, 'pyproject.toml');
+  try {
+    await fs.access(pyprojectTomlPath);
+    const pyprojectData = await fs.readFile(pyprojectTomlPath, 'utf8');
+    let pyproject = toml.parse(pyprojectData);
+
+    pyproject = {
+      ...(pyproject ?? {}),
+      project: {
+        ...(pyproject.project ?? {}),
+        version: newVersion,
+      },
+    };
+
+    await fs.writeFile(pyprojectTomlPath, toml.stringify(pyproject));
+    return;
+  } catch (error) {
+    logging.warning(`pyproject.toml not found or invalid in ${projectPath}; moving on...`, error);
   }
 
-  const setupPyPath = path.join(projectPath, 'setup.py');
-  if (await fs.pathExists(setupPyPath)) {
-    try {
-      return updateSetupPyVersion(setupPyPath, newVersion);
-    } catch (error) {
-      logging.error(`Failed to update Python project version: ${error.message}`);
-    }
+  const poetryTomlPath = path.join(projectPath, 'poetry.toml');
+  try {
+    await fs.access(poetryTomlPath);
+    const poetryData = await fs.readFile(poetryTomlPath, 'utf8');
+    let pyproject = toml.parse(poetryData);
+
+    pyproject = {
+      ...(pyproject ?? {}),
+      tool: {
+        ...(project?.tool ?? {}),
+        poetry: {
+          ...pyproject.tool?.poetry,
+          version: newVersion,
+        },
+      },
+    };
+
+    await fs.writeFile(poetryTomlPath, toml.stringify(pyproject));
+    return;
+  } catch (error) {
+    logging.warning(`poetry.toml not found or invalid in ${projectPath}; moving on...`, error);
   }
 
-  const setupCfgPath = path.join(projectPath, 'setup.cfg');
-  if (await fs.pathExists(setupCfgPath)) {
+  const setupMatches = {
+    'setup.py': /version\s*=\s*["']?([^"'\s]+)["']?/,
+    'setup.cfg': /version\s*=\s*["']?([^"'\s]+)["']?/,
+    '__init__.py': /__version__\s*=\s*["']?([^"'\s]+)["']?/,
+  };
+
+  // Check for setup.py and setup.cfg using regex
+  for (const file of Object.keys(setupMatches)) {
+    const filePath = path.join(projectPath, file);
     try {
-      return updateSetupCfgVersion(setupCfgPath, newVersion);
-    } catch (error) {
-      logging.error(`Failed to update Python project version: ${error.message}`);
-    }
-  }
-
-  const {name: packageName} = detect(projectPath);
-  const initPaths = [
-    path.join(projectPath, '__init__.py'),
-    path.join(projectPath, 'src/__init__.py'),
-    path.join(projectPath, `${packageName}/__init__.py`),
-    path.join(projectPath, `src/${packageName}/__init__.py`),
-  ];
-
-  for (const initPath of initPaths) {
-    if (await fs.pathExists(initPath)) {
-      try {
-        return updateInitPyVersion(initPath, newVersion);
-      } catch (error) {
-        logging.error(`Failed to update Python project version: ${error.message}`);
+      await fs.access(filePath);
+      let content = await fs.readFile(filePath, 'utf8');
+      if (file === '__init__.py') {
+        content = content.replace(setupMatches[file], `__version__ = "${newVersion}"`);
+      } else {
+        content = content.replace(setupMatches[file], `version = "${newVersion}"`);
       }
+
+      return fs.writeFile(projectPath, content);
+    } catch (error) {
+      logging.warning(`${file} not found or invalid in ${projectPath}; moving on...`, error);
     }
   }
 
   logging.error(`No version file found in the Python project at ${projectPath}`);
-};
-
-/**
- *
- * @param {string} pyprojectPath
- * @param {string} newVersion
- */
-const updatePyProjectVersion = async (pyprojectPath, newVersion) => {
-  const content = await fs.readFile(pyprojectPath, 'utf8');
-  const pyprojectData = toml.parse(content);
-
-  if (pyprojectData.tool?.poetry) {
-    pyprojectData.tool.poetry.version = newVersion;
-  }
-  if (pyprojectData.project) {
-    pyprojectData.project.version = newVersion;
-  }
-  if (pyprojectData.tool?.flit?.metadata) {
-    pyprojectData.tool.flit.metadata.version = newVersion;
-  }
-
-  const updatedContent = toml.stringify(pyprojectData);
-  await fs.writeFile(pyprojectPath, updatedContent);
-};
-
-/**
- *
- * @param {string} initPath
- * @param {string} newVersion
- */
-const updateSetupPyVersion = async (setupPyPath, newVersion) => {
-  const content = await fs.readFile(setupPyPath, 'utf8');
-  const updatedContent = content.replace(/(version\s*=\s*)["']([^"']*)["']/, `$1"${newVersion}"`);
-
-  await fs.writeFile(setupPyPath, updatedContent);
-};
-
-/**
- *
- * @param {string} initPath
- * @param {string} newVersion
- */
-const updateSetupCfgVersion = async (setupCfgPath, newVersion) => {
-  const content = await fs.readFile(setupCfgPath, 'utf8');
-  const updatedContent = content.replace(/(version\s*=\s*)([^\s;]*)/, `$1${newVersion}`);
-
-  await fs.writeFile(setupCfgPath, updatedContent);
-};
-
-/**
- *
- * @param {string} initPath
- * @param {string} newVersion
- */
-const updateInitPyVersion = async (initPath, newVersion) => {
-  const content = await fs.readFile(initPath, 'utf8');
-  if (content.includes('version') || content.includes('__version__')) {
-    const updatedContent = content.replace(/(__|)version(__|)\s*=\s*["']([^"']*)["']/, `$1version$2 = "${newVersion}"`);
-
-    // Only write if we actually changed something
-    if (content !== updatedContent) {
-      await fs.writeFile(initPath, updatedContent);
-    }
-  }
 };
