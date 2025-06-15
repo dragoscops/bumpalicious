49cf0a602ff5924ecae3d0fd17cf2d086b4bd65b
Test User <dragos.cirjan@gmail.com>
2025-06-15 17:20:38 +0300

    chore: debug 1749997238

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -6,9 +6,11 @@ import pino from 'pino';
  */
 
 export const logger = pino({
+  level: process.env.LOG_LEVEL || 'info',
   transport: {
     target: 'pino-pretty',
     options: {
+      destination: 1,
       colorize: true,
     },
   },
